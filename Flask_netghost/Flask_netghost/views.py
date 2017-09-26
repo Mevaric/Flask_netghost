a8e02d1badc3cba5c69b0c2267c77f9560624d17
Mevaric <vladislav.khripunov@gmail.com>
2017-09-27 02:57:35 +0700

    add post file

--- Flask_netghost/Flask_netghost/views.py
+++ Flask_netghost/Flask_netghost/views.py
@@ -5,6 +5,47 @@ Routes and views for the flask application.
 from datetime import datetime
 from flask import render_template
 from Flask_netghost import app
+from flask_sqlalchemy import SQLAlchemy
+from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin, login_required
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+
+db = SQLAlchemy(app)
+
+
+class Blogpost(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(50))
+    subtitle = db.Column(db.String(50))
+    data_posted = db.Column(db.DateTime)
+    content = db.Column(db.Text)
+
+roles_users = db.Table('roles_users',
+        db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+        db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
+
+
+# Setup Flask-Security
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
+
+db.create_all()
 
 @app.route('/')
 @app.route('/home')
@@ -35,3 +76,8 @@ def about():
         year=datetime.now().year,
         message='Your application description page.'
     )
+
+@app.route('/created_post')
+@roles_required('admin','creater','author')
+def post():
+    return render_template('post.html')
